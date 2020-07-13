e24492ce17338f827ab6ba0e76fc5ff9caa2d624
unknown <generation@email.com>
2020-07-13 11:53:37 -0300

    exemplo de orientação objeto

---
+++ POO/POO 1/POO1 pessoa nome/Nome.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class Nome {
+	public static void main(String args[])
+	{
+		Pessoa pessoa1 = new Pessoa ("Maria","","de Oliveira");
+		System.out.println(pessoa1.getNomeCompletoPessoa());
+		System.out.println(pessoa1.getPrimeiroNome());
+	}
+			
+}
